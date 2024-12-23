9a10fab5cc517c7fe556324402b6d4bf34ea59e4
codelily <coldelily98@naver.com>
2024-12-23 09:31:07 +0900

    [FE] Feat: 배포 TEST host IP 변경

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0", // 호스트를 localhost로 설정
-    port: 5173, // 포트 설정
+    port: 80, // 포트 설정
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // Spring Boot 서버 주소
+        target: "http://211.188.49.199:80", // Spring Boot 서버 주소
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // "/api" 제거
       },
