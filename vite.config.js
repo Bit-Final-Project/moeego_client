7fa10cebb6ab3fd249eecd73f14cb063af64332e
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-23 17:28:22 +0900

    [FE] Ref: vite.config.js 원복

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "175.45.194.160",  // 배포 서버의 실제 IP 주소
-    port: 80,  // 포트 설정 (80은 기본 HTTP 포트)
-    // proxy: { }  // Vite에서의 /api 프록시 설정을 제거
+    host: "175.45.194.160", // 호스트를 localhost로 설정
+    port: 80, // 포트 설정
+    #proxy: {
+      "/api": {
+        target: "http://211.188.49.199:8080", // Spring Boot 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // "/api" 제거
+      },
+    },
   },
   optimizeDeps: {
-    include: ["jwt-decode"],  // jwt-decode 모듈을 번들링
+    include: ["jwt-decode"], // jwt-decode 모듈을 번들링
   },
 });
